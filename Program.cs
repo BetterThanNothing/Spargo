fcac330e739d9cabf39b7900d285d777fd0df845
BetterThanNothing <newbroadgate@gmail.com>
2016-07-15 12:04:53 +0300

    1st
    first commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DestinationPoints
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var source = new List<Tuple<string, string>>
+            {
+                new Tuple<string, string>("Мельбурн", "Кельн"),
+                new Tuple<string, string>("Берлин", "Токио"),
+                new Tuple<string, string>("Москва", "Париж"),
+                new Tuple<string, string>("Кельн", "Москва"),
+                new Tuple<string, string>("Париж", "Берлин")
+            };
+
+            var result = GetRoute(source);
+            foreach (var item in result) Console.WriteLine("{0} - {1}", item.Item1, item.Item2);
+            Console.ReadKey();
+        }
+
+        //примерная сложность N^2
+        public static List<Tuple<string, string>> GetRoute(List<Tuple<string, string>> input)
+        {
+            var d = input.ToDictionary(x => x.Item1, v => v);
+            var first = d.Single(x => d.Values.All(v => v.Item2 != x.Key)).Value;
+            var output = new List<Tuple<string, string>> { first };
+            var temp = first;
+            while (d.TryGetValue(temp.Item2, out temp)) output.Add(temp);
+            return output;
+        }
+    }
+}
---
+++ UnitTest1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using DestinationPoints;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var source = new List<Tuple<string, string>>
+            {
+                new Tuple<string, string>("Мельбурн", "Кельн"),
+                new Tuple<string, string>("Берлин", "Токио"),
+                new Tuple<string, string>("Москва", "Париж"),
+                new Tuple<string, string>("Кельн", "Москва"),
+                new Tuple<string, string>("Париж", "Берлин")
+            };
+
+            var target = new List<Tuple<string, string>>
+            {
+                new Tuple<string, string>("Мельбурн", "Кельн"),
+                new Tuple<string, string>("Кельн", "Москва"),
+                new Tuple<string, string>("Москва", "Париж"),
+                new Tuple<string, string>("Париж", "Берлин"),
+                new Tuple<string, string>("Берлин", "Токио")
+            };
+
+            var result = Program.GetRoute(source);
+            CollectionAssert.AreEqual(target, result);
+        }
+    }
+}
